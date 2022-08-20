6dfe86f72eda381a77a2a0238d2c949ed2464c05
Nika Darab <nika@brivity.com>
2022-08-20 16:26:50 -0500

    added logo

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 const SearchBar = () => {
-    return (
-        <div className="browse-button animate__animated animate__bounceInRight header">
-        <div className="search-container">
-          <input type="text" placeholder="Search..." />
-          <div className="search" />
-        </div>
+  return (
+    <div className="browse-button animate__animated animate__bounceInRight header">
+        <div className="logo"/>
+      <div className="search-container">
+        <input type="text" placeholder="Search..." />
+        <div className="search" />
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default SearchBar;
