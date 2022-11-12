92eec659e98b1dacd45514bfa026eb3063f85f98
Nika Darab <nika@brivity.com>
2022-11-12 01:30:03 -0600

    Removed unused route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Search from "./Search";
-import Main from "./Main";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    //get all the data at once
     fetch("https://ghibliapi.herokuapp.com/films")
       .then((response) => response.json())
       .then((data) => {
@@ -19,21 +17,11 @@ function App() {
       });
   }, []);
 
-  let getAllData = () => {
-    fetch(`https://ghibliapi.herokuapp.com/films`)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main/>} exact/>
-        <Route path="/search" element={<Search/>} />
+        <Route path="/" element={<Search />} exact />
+        {/* <Route path="/search" element={<Search/>} /> */}
       </Routes>
     </BrowserRouter>
   );
